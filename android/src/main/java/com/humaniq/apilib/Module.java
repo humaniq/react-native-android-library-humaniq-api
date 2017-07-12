7b68465cd69474e591a1b6f8ccebd1f8688878fe
Alex Gritsay <aleksandr.gritsay@gmail.com>
2017-07-12 11:21:26 +0600

    sample request

--- android/src/main/java/com/humaniq/apilib/Module.java
+++ android/src/main/java/com/humaniq/apilib/Module.java
@@ -2,13 +2,19 @@ package com.humaniq.apilib;
 
 import android.widget.Toast;
 
+import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.humaniq.apilib.models.AddressState;
+import com.humaniq.apilib.models.response.BaseResponse;
 import com.humaniq.apilib.services.ServiceBuilder;
 
 import java.util.HashMap;
 import java.util.Map;
+
+import retrofit2.Call;
+import retrofit2.Response;
 
 public class Module extends ReactContextBaseJavaModule {
 
@@ -38,4 +44,27 @@ public class Module extends ReactContextBaseJavaModule {
   public void show(String message, int duration) {
     Toast.makeText(getReactApplicationContext(), message, duration).show();
   }
+
+  @ReactMethod
+  public void stateResponseCall(Callback errorCallback,
+                                final Callback successCallbac) {
+
+    Call<BaseResponse<AddressState>> stateResponseCall =
+            ServiceBuilder.getProfileService().
+                    getAddressState("0x1111111111111111111111111111111111111111");
+
+    stateResponseCall.enqueue(new retrofit2.Callback<BaseResponse<AddressState>>() {
+      @Override
+      public void onResponse(Call<BaseResponse<AddressState>> call, Response<BaseResponse<AddressState>> response) {
+        successCallbac.invoke(response.body());
+      }
+
+      @Override
+      public void onFailure(Call<BaseResponse<AddressState>> call, Throwable t) {
+        successCallbac.invoke(t.getLocalizedMessage());
+      }
+    });
+
+  }
+
 }
