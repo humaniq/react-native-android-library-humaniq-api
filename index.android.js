b4db0b6020660b3a9383cf017b9725374b67bffe
ognev <zair407@gmail.com>
2017-07-12 17:20:23 +0500

    adding transactions module

--- index.android.js
+++ index.android.js
@@ -2,6 +2,6 @@
 
 import { NativeModules } from 'react-native'
 // name as defined via ReactContextBaseJavaModule's getName
-module.exports = NativeModules.HumaniqProfileApiLib
+module.exports = NativeModules.HumaniqApiLib
 
 
