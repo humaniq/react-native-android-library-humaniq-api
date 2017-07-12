d26ffb1ef62da02212099579a66515ca744e94e7
ognev <zair407@gmail.com>
2017-07-12 17:19:42 +0500

    Implemented bridge to react 1. exporting HumaniqProfileApiLib

--- android/src/main/java/com/humaniq/apilib/Module.java
+++ android/src/main/java/com/humaniq/apilib/Module.java
@@ -3,14 +3,24 @@ package com.humaniq.apilib;
 import android.widget.Toast;
 
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.WritableArray;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.bridge.WritableNativeArray;
+import com.humaniq.apilib.constructor.ModelUtils;
 import com.humaniq.apilib.models.AddressState;
+import com.humaniq.apilib.models.Transaction;
 import com.humaniq.apilib.models.response.BaseResponse;
 import com.humaniq.apilib.services.ServiceBuilder;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import retrofit2.Call;
@@ -65,6 +75,45 @@ public class Module extends ReactContextBaseJavaModule {
       }
     });
 
+  }
+
+  @ReactMethod
+  public void getTransactions(String id, final Promise promise) {
+    ServiceBuilder.getProfileService().getAddressTransactions(id)
+            .enqueue(new retrofit2.Callback<BaseResponse<List<Transaction>>>() {
+              @Override
+              public void onResponse(Call<BaseResponse<List<Transaction>>> call,
+                                     Response<BaseResponse<List<Transaction>>> response) {
+
+                try {
+                  WritableArray array = new WritableNativeArray();
+
+                  for(Transaction transaction : response.body().data) {
+                    WritableMap collectionTransaction = ModelUtils.convertJsonToMap(new JSONObject(transaction.toJsonString()));
+                    collectionTransaction.putString("transaction", collectionTransaction.getString("transaction"));
+                    collectionTransaction.putInt("type", collectionTransaction.getInt("type"));
+                    collectionTransaction.putInt("status", collectionTransaction.getInt("status"));
+                    collectionTransaction.putInt("amount", collectionTransaction.getInt("amount"));
+                    collectionTransaction.putInt("receivedTransactions", collectionTransaction.getInt("receivedTransactions"));
+                    collectionTransaction.putInt("sentTransactions", collectionTransaction.getInt("sentTransactions"));
+                    collectionTransaction.putInt("invalidTransactions", collectionTransaction.getInt("invalidTransactions"));
+                    collectionTransaction.putInt("timestamp", collectionTransaction.getInt("timestamp"));
+                    array.pushMap(collectionTransaction);
+                  }
+
+                  promise.resolve(array);
+                } catch (JSONException e) {
+                  promise.reject("", e);
+                }
+              }
+
+              @Override
+              public void onFailure(Call<BaseResponse<List<Transaction>>> call,
+                                    Throwable t) {
+
+                promise.reject("", t);
+              }
+            });
   }
 
 }
