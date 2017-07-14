30fb2dc0b34c7823bf097e2cea9d901b6c5f4487
ognev <zair407@gmail.com>
2017-07-14 16:17:23 +0500

    export modules

--- index.android.js
+++ index.android.js
@@ -2,7 +2,7 @@
 
 import { NativeModules } from 'react-native'
 // name as defined via ReactContextBaseJavaModule's getName
-const { HumaniqApiLib } = NativeModules
-const { HumaniqProfileApiLib } = NativeModules
+export const { HumaniqApiLib } = NativeModules
+export const { HumaniqProfileApiLib } = NativeModules
 //module.exports = NativeModules.HumaniqApiLib
 
