98fabdda36ca9766cbc8ed28c45d4a621a4d6712
ognev <zair407@gmail.com>
2017-07-17 20:56:29 +0500

    adding opportunity to download desired files

--- android/src/main/java/com/humaniq/apilib/DownloadModule.java
+++ android/src/main/java/com/humaniq/apilib/DownloadModule.java
@@ -7,46 +7,21 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.database.Cursor;
 import android.net.Uri;
-import android.util.Log;
-import android.widget.ImageView;
-import android.widget.Toast;
 
 import com.facebook.common.internal.Files;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.WritableArray;
 import com.facebook.react.bridge.WritableMap;
-import com.facebook.react.bridge.WritableNativeArray;
 import com.facebook.react.bridge.WritableNativeMap;
-import com.humaniq.apilib.constructor.ModelConverterUtils;
-import com.humaniq.apilib.services.restService.ServiceBuilder;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.util.HashMap;
 import java.util.Map;
 
-/*
-*
-* import HumaniqApiLib from 'react-native-android-library-humaniq-api'
-import HumaniqProfileApiLib from 'react-native-android-library-humaniq-api'
-* HumaniqProfileApiLib.getTransactions('0x1111111111111111111111111111111111111111').then((addressState) => {
-      // console.log(array);
-      console.warn(JSON.stringify(addressState))
-        //HumaniqApiLib.show(addressState.address);
-    });
 
-
-        HumaniqApiLib.getTransactions('0x1111111111111111111111111111111111111111').then((array) => {
-          // console.log(array);
-          console.warn(JSON.stringify(array))
-            // HumaniqApiLib.show(array[0].name);
-        });*/
 public class DownloadModule extends ReactContextBaseJavaModule {
     private long enqueue;
     private DownloadManager dm;
@@ -125,11 +100,8 @@ public class DownloadModule extends ReactContextBaseJavaModule {
         return new HashMap<>();
     }
 
-    // http://clips.vorwaerts-gmbh.de/VfE_html5.mp4
-
     @ReactMethod
     public void downloadVideoFile(String uri, final Promise downloadPromise) {
-
         if(Prefs.isUriAlreadyDownloaded(uri)) {
             WritableMap localUri = new WritableNativeMap();
             localUri.putString("uri", Prefs.getLocalUri());
@@ -144,34 +116,14 @@ public class DownloadModule extends ReactContextBaseJavaModule {
                 dm = (DownloadManager) getReactApplicationContext()
                         .getSystemService(getReactApplicationContext().DOWNLOAD_SERVICE);
                 DownloadManager.Request request = new DownloadManager.Request(
-                        Uri.parse("http://clips.vorwaerts-gmbh.de/VfE_html5.mp4"));
+                        Uri.parse(uri));
                 request.setVisibleInDownloadsUi(false);
                 request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_HIDDEN);
                 request.allowScanningByMediaScanner();
 
                 enqueue = dm.enqueue(request);
-            } else {
-                Log.w("null", "null context");
             }
         }
-    }
-
-    @ReactMethod
-    public void resumeDownloadVideoFile(String id, final Promise promise) {
-        // MOCK data!
-        try {
-            WritableMap balanceMock =
-                    ModelConverterUtils.convertJsonToMap(new JSONObject(" {" +
-                            "    \"HMQ\": 190.6," +
-                            "    \"USD\": 22.62422" +
-                            "  }"));
-
-            promise.resolve(balanceMock);
-
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
     }
 
 }
