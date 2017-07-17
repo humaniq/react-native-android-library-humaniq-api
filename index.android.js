fe1d809dfbce8373fb4559ce35bcc06ed4624974
ognev <zair407@gmail.com>
2017-07-17 18:34:10 +0500

    few modules bug

--- index.android.js
+++ index.android.js
@@ -4,8 +4,8 @@ import { NativeModules } from 'react-native'
 // name as defined via ReactContextBaseJavaModule's getName
 
 
-module.exports = NativeModules.HumaniqApiLib
-module.exports = NativeModules.HumaniqProfileApiLib
+//module.exports = NativeModules.HumaniqApiLib
+//module.exports = NativeModules.HumaniqProfileApiLib
 module.exports = NativeModules.HumaniqDownloadFileLib
-module.exports = NativeModules.HumaniqContactsApiLib
+//module.exports = NativeModules.HumaniqContactsApiLib
 
